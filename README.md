34ae9be440129de5749e84010c03558c7dc43c2f
Anirudh Kanakatte <ani9526@gmail.com>
2020-01-16 20:09:26 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# MicroPosts
+
+A single user social media app for creating and viewing posts. Posts contain a title and a body and the individual posts can be edited and deleted.
+
+## Usage
+
+### Building the application
+
+Use webpack to build the application
+
+```
+$ npm run build
+```
+
+Start the json-server application which contains all the post data
+
+```
+$ npm run json:server
+```
+
+Run webpack-dev-server to serve the application
+
+```
+$ npm run start:dev
+```
+
+## Technologies Used
+
+- JavaScript
+- JavaScript modules
+- Webpack
+- Babel
+- Json-server: Fake REST API tool
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: ["@babel/polyfill", "./src/index.js"]
+  },
+  mode: "production",
+  devServer: {
+    open: true
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "source.html"
+    })
+  ]
+};
